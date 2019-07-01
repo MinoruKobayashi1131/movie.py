30f9e722a20f4826de36d62156acc92c11b697fc
Minoru Kobayashi <39611917+MinoruKobayashi1131@users.noreply.github.com>
2019-07-01 14:40:53 +0900

    20190701

--- movie.py
+++ movie.py
@@ -5,6 +5,12 @@ import roslib
 from cv_bridge import CvBridge
 import numpy as np
 from sensor_msgs.msg import Image
+#from PIL import Image as ImagePIL
+#from PIL import ImageOps
+import time
+import pycuda.gpuarray as gpuarray
+import pycuda.driver as cuda
+import pycuda.autoinit
 
 delay = 30
 window_name = 'frame'
@@ -15,7 +21,7 @@ rospy.init_node('image_feature', anonymous=True)
 
 print(args[1])
 file_path = args[1]
-image_pub = rospy.Publisher("image_data",Image,queue_size = 10)
+image_pub = rospy.Publisher("image_data",Image,queue_size = 100)
 
 
 cap = cv2.VideoCapture(file_path)
@@ -27,7 +33,9 @@ rate = rospy.Rate(30)  # 30hz
 while True:
     ret, frame = cap.read()
     if ret:
-        cv2.imshow(window_name, frame)
+        scaled = cv2.resize(frame, dsize=(320, 240))       
+        cv2.imshow(window_name, scaled)
+
         # make bridge
         bridge = CvBridge()
         msg = bridge.cv2_to_imgmsg(frame, encoding="bgr8")
@@ -40,4 +48,5 @@ while True:
     rate.sleep()
 
 cv2.destroyWindow(window_name)
+
 
